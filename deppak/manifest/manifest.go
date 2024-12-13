f4b7f0bbf727771ff76706d0ae33b6372927e3a0
Tai Kedzierski <dch.tai@gmail.com>
2024-12-13 22:22:32 +0000

    Little progress with JSON parsing ...

--- deppak/manifest/manifest.go
+++ deppak/manifest/manifest.go
@@ -3,6 +3,8 @@ package manifest
 import (
     "encoding/json"
     "fmt"
+    "errors"
+    "io/ioutil"
 )
 
 type SyncItem struct {
@@ -14,20 +16,39 @@ type SyncItem struct {
 
 func LoadManifest(path string) (*SyncItem, error) {
     // NOTE - when loading the manifest, ensure there are no duplicate hashes (excl "-")
-    // TODO - read from file
-    fmt.Println(path)
-    return extractManifest(`{"url":"there", "hash":"abcd1234", "dest":"mods/test_mod", "src":"subdir"}`)
+    content, err := ioutil.ReadFile(path)
+    if err != nil {
+        return nil, err
+    }
+    // now we need to extract... a LIST of SyncItems ...
+    // We might need to load the file to some interface{} , assert it is some sort of list
+    //   and then coerce each into a SyncItem ...
+    return extractManifest(content)
 }
 
 
 func extractManifest(json_data string) (*SyncItem, error) {
     var data *SyncItem
 
+    // Interestingly:
+    //   unknown keys are ignored (value discarded)
+    //   missing keys are non-populated
+    //   null-values are non-populated
+    // Non-populated values leave the struct at default values
+    // So null in JSON leads to a struct string field becoming  ""
     err := json.Unmarshal([]byte(json_data), &data)
 
     if err != nil {
         return nil, err
     }
+
+    if data.Dest == "" {
+        data.Dest = "."
+    }
+
+    if data.Url == "" {return nil, errors.New("Missing url")}
+    if data.Src == "" {return nil, errors.New("Missing src")}
+    if data.Hash == "" {return nil, errors.New("Missing hash")}
 
     return data, nil
 }
--- deppak/manifest/manifest_test.go
+++ deppak/manifest/manifest_test.go
@@ -7,9 +7,10 @@ import (
 
 func Test_extractManifest(t *testing.T) {
     hash_val := "abcd1234"
+    dest_val := "mods/test_mod"
 
     // NOTE - this is the old format. We need to expect a "deploy" key with a map of source to list of dest
-    data, err := extractManifest(fmt.Sprintf(`{"url":"there", "hash":"%s", "dest":"mods/test_mod", "src":"subdir"}`, hash_val))
+    data, err := extractManifest(fmt.Sprintf(`{"url":"there", "hash":"%s", "dest":"%s", "src":"subdir"}`, hash_val, dest_val))
 
     if err != nil {
         t.Errorf("%s", err )
@@ -17,5 +18,42 @@ func Test_extractManifest(t *testing.T) {
 
     if data.Hash != hash_val {
         t.Errorf("Expected %s , got %s", hash_val, data.Hash)
+    }
+
+    if data.Dest != dest_val {
+        t.Errorf("Expected %s, got '%s'", dest_val, data.Dest)
+    }
+
+    // Omitting dest as it is optional
+    data2, err2 := extractManifest(fmt.Sprintf(`{"url":"there", "hash":"%s", "src":"subdir"}`, hash_val))
+
+    if err2 != nil {
+        t.Errorf("%s", err2 )
+    }
+
+    if data2.Dest != "." {
+        t.Errorf("Expected %s, got '%s'", dest_val, data2.Dest)
+    }
+
+    data3, err3 := extractManifest(`{"url":"oops"}`)
+    if err3 == nil {
+        t.Errorf("Should have failed on incomplete input! -> %s", data3)
+    }
+
+    /*
+    // This actually succeeds - unaccounted-for keys are simply ignored.
+    data4, err4 := extractManifest(`{"url":"ok", "hash":"what", "src": "here", "damn":"oops"}`)
+    if err4 == nil {
+        t.Errorf("Should have failed on bad input! -> %s", data4)
+    } else {
+        fmt.Printf("Got expected Err = %s\n", err4)
+    }
+    // */
+    
+    data5, err5 := extractManifest(`{"url":null}`)
+    if err5 == nil {
+        t.Errorf("Should have failed on null data input! -> %s", data5)
+    } else {
+        fmt.Printf("Got expected Err = %s\n", err5)
     }
 }
