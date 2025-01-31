88eca6250c95a9045f8e859ab5a574eddc1e091f
Tai Kedzierski <dch.tai@gmail.com>
2025-01-31 12:26:26 +0000

    Perform validation on cli arguments

--- deppak/cli/cliargs.go
+++ deppak/cli/cliargs.go
@@ -45,15 +45,26 @@ func ParseCliArgs() DepPakArgs {
     flag.Parse()
     positionals := flag.Args()
 
-    if len(positionals) == 0 {
-        fmt.Printf("Expecting MANIFEST argument")
-        os.Exit(nstatus.ARGUMENT_ERROR)
+    if len(positionals) != 1 {
+        fail(nstatus.ARGUMENT_ERROR, "Expecting one MANIFEST argument")
     }
 
     if token, ok := checkTrailingFlags(positionals); !ok {
-        fmt.Printf("Found flag '%s' . Place all flags before positional arguments.\n", token)
-        os.Exit(nstatus.ARGUMENT_ERROR)
+        fail(nstatus.ARGUMENT_ERROR, "Found flag '%s' . Place all flags before positional arguments.\n", token)
     }
 
-    return DepPakArgs{unpack_root, positionals[0]}
+    pos_info, err := os.Stat(positionals[0])
+    if err != nil { fail(nstatus.ARGUMENT_ERROR, "Could not access '%s': %s", positionals[0], err); }
+    if pos_info.IsDir() { fail(nstatus.ARGUMENT_ERROR, "'%s' is a directory, file required", positionals[0]) }
+
+    urt_info, err := os.Stat(unpack_root)
+    if err != nil { fail(nstatus.ARGUMENT_ERROR, "Could not access '%s': %s", unpack_root, err); }
+    if !urt_info.IsDir() { fail(nstatus.ARGUMENT_ERROR, "'%s' is not a directory", unpack_root) }
+
+    return DepPakArgs{positionals[0], unpack_root}
+}
+
+func fail(status int, message string, tokens... any) {
+    fmt.Println(fmt.Sprintf(message, tokens...))
+    os.Exit(status)
 }
